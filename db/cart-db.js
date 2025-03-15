4afb2cbc3dce53a23a1183dd705b7be2cb08e419
parsalemi <parsalemi@outlook.com>
2025-03-15 13:45:04 +0330

    fix: query for cart

--- db/cart-db.js
+++ db/cart-db.js
@@ -1,24 +1,36 @@
 import knex from './knex.js';
 
-function getCart(id){
-  return knex('carts').where('id', id).select('*');
+function getCart(userId){
+  return knex('carts').select('id', 'userid', 'purchased', 'order').where('userid', userId).where('purchased', 0).first();
 }
 
-function addToCart(order){
-  return knex('carts').insert(order);
+function addToCart(userId, order){
+  return knex('carts').where('userid', userId).insert({
+    'order': order,
+    'userid': userId,
+    'purchased': 0,
+  });
 }
 
-function deleteCart(id){
-  return knex('carts').where('id', id).del();
+function purchaseCart(userId){
+  return knex('carts').where('userid', userId).update({
+    'purchased': 1,
+    'userid': userId,
+  });
 }
 
-function updateCart(id, order){
-  return knex('carts').where('id', id).update(order);
+function deleteCart(userId){
+  return knex('carts').where('userid', userId).where('purchased', 0).del();
+}
+
+function updateCart(userId, order){
+  return knex('carts').where('userid', userId).where('purchased', 0).update({'order': order});
 }
 
 export default {
   getCart,
   addToCart,
+  purchaseCart,
   deleteCart,
   updateCart,
 }
