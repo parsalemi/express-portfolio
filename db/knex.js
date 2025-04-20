da18d9b3d803e2b5cc784b76023504b8c6369c35
parsalemi <parsalemi@outlook.com>
2025-04-21 02:33:07 +0330

    fix: database has deployed to cloudflare; new knex config for dev and prod env

--- db/knex.js
+++ db/knex.js
@@ -1,13 +1,41 @@
 import knex from 'knex';
+import fetch from 'cross-fetch';
 
-const connectedKnex = knex({
-  client: "sqlite3",
+const d1Config = {
+  client: 'sqlite3',
   connection: {
-    filename: process.env.NODE_ENV === 'production'
-    ? "/tmp/db.sqlite3"
-    : "./src/db.sqlite3"
+    filename: ':memory:',
+    async query({sql, bindings}){
+      const response = await fetch(
+        `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_Account_ID}/d1/database/${process.env.CF_DB_ID}/query`,
+        {
+          method: 'POST',
+          headers: {
+            'Authorization': `Bearer ${process.env.CF_API_TOKEN}`,
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            sql,
+            params: bindings
+          })
+        }
+      );
+      const data = await response.json();
+      return data.result;
+    }
+  },
+  useNullAsDefault: true
+}
+
+const localConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './src/db.sqlite3'
   }
-});
+}
+
+const config = process.env.NODE_ENV == 'production' ? d1Config : localConfig;
+const connectedKnex = knex(config);
 
 export default connectedKnex;
 // module.exports = connectedKnex;
